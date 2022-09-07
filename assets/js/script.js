dd7a5699be55233afcc2d4a109e889a87a17fbdb
AndypSheridan <andy.p.sheridan@gmail.com>
2022-09-07 11:13:56 +0000

    Create function fr background line

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Extract context to allow interaction with canvas
+//Extract context to allow interaction with canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -7,4 +7,12 @@ let scoreSFX = new Audio("https://archive.org/download/classiccoin/classiccoin.w
 let gameOverSFX = new Audio("https://archive.org/download/smb_gameover/smb_gameover.wav");
 let jumpSFX = new Audio("https://archive.org/download/jump_20210424/jump.wav");
 
-
+//Create horizontal line across width of canvas
+function drawBackgroundLine() {
+    ctx.beginPath();
+    ctx.moveTo(0,400);
+    ctx.lineTo(600,400);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+}
