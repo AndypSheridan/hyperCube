ce07da938ddd453d0214cecf5b51eabd92122608
AndypSheridan <andy.p.sheridan@gmail.com>
2022-09-30 18:05:08 +0000

    Comment out touch event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -334,7 +334,7 @@ addEventListener("keydown", e => {
 })
 
 //Touch jump event listener
-addEventListener("touchstart", e => {
+/*addEventListener("touchstart", e => {
     if(e.type === "touchstart"){
         e.preventDefault();
         if(!player.shouldJump){
@@ -344,4 +344,4 @@ addEventListener("touchstart", e => {
             canScore = true;
         }
     }
-});
+});*/
