0079846752d0ccb56f60ae6c567c12ba9de7bb09
AndypSheridan <andy.p.sheridan@gmail.com>
2022-09-30 15:05:57 +0000

    Remove game over sfx and function

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,11 +53,8 @@ myAudio.onpause = function() {
     isPlaying = false;
 }
 
-
-
 //Game SFX
 let scoreSFX = new Audio("https://archive.org/download/classiccoin/classiccoin.wav");
-let gameOverSFX = new Audio("https://archive.org/download/smb_gameover/smb_gameover.wav");
 let jumpSFX = new Audio("https://archive.org/download/jump_20210424/jump.wav");
 
 //Used for 'setInterval'
@@ -282,7 +279,6 @@ function animate() {
         arrayBlock.slide();
         //End game as player and enemy have collided
         if(squaresColliding(player, arrayBlock)){
-            gameOverSFX.play();
             cardScore.textContent = score;
             card.style.display = "block";
 
